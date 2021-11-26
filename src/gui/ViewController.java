0843c65e63a99e9413d37fbbd264a50611d794d1
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2021-11-26 14:16:10 +0100

    Version with gui
    I need to add model and the mvc should be done

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,4 +1,51 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class ViewController {
+    public void handleFilter(ActionEvent event) {
+    }
+
+    public void handleVolume(MouseEvent mouseEvent) {
+    }
+
+    public void handleNext(ActionEvent event) {
+    }
+
+    public void handlePrevious(ActionEvent event) {
+    }
+
+    public void handlePlay(ActionEvent event) {
+    }
+
+    public void handleDeletePlaylist(ActionEvent event) {
+    }
+
+    public void handleEditPlaylist(ActionEvent event) {
+    }
+
+    public void handleNewPlaylist(ActionEvent event) {
+    }
+
+    public void handleRemoveFromPlaylist(ActionEvent event) {
+    }
+
+    public void handleMoveDown(ActionEvent event) {
+    }
+
+    public void handleMoveUp(ActionEvent event) {
+    }
+
+    public void handleSongToPlaylist(ActionEvent event) {
+    }
+
+    public void handleDeleteSong(ActionEvent event) {
+    }
+
+    public void handleEditSong(ActionEvent event) {
+    }
+
+    public void handleNewSong(ActionEvent event) {
+    }
 }
