f3c72b632c34f81ac45d610c956e0c2ff192ecde
ChristianHesberg <chri59kx@easv365.dk>
2021-12-01 13:31:04 +0100

    Update TableViewPlaylistsModel.java

--- src/mytunes/gui/model/TableViewPlaylistsModel.java
+++ src/mytunes/gui/model/TableViewPlaylistsModel.java
@@ -1,4 +1,35 @@
 package mytunes.gui.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import mytunes.be.Playlist;
+import mytunes.be.Song;
+
 public class TableViewPlaylistsModel {
+
+    ObservableList<Playlist> playlistList;
+
+    public TableViewPlaylistsModel()
+    {
+        playlistList = FXCollections.observableArrayList();
+    }
+
+    public void addPlaylist(Playlist playlist)
+    {
+        playlistList.add(playlist);
+    }
+
+    public ObservableList<Playlist> getPlaylistList()
+    {
+        return this.playlistList;
+    }
+
+    public void edit(Playlist selectedPlaylist, Playlist editedPlaylist){
+        playlistList.set(playlistList.indexOf(selectedPlaylist), editedPlaylist);
+    }
+
+
+    public void deletePlaylist(Playlist playlist) {
+        playlistList.remove(playlist);
+    }
 }
